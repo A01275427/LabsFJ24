c571d0e1e9e3f8d0edb0ce1775229756ec910f98
FaKeArtur27 <arsanmx@gmail.com>
2024-02-26 23:04:11 -0600

    botones

--- lab.js
+++ lab.js
@@ -143,6 +143,23 @@ function calcularTotal() {
 */
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.getElementById('toggleContentBtn');
+    var content = document.getElementById('content');
+
+    toggleButton.addEventListener('click', function() {
+        if (content.style.display === 'none') {
+            content.style.display = 'block';
+            toggleButton.textContent = 'Ocultar LAB';
+        } else {
+            content.style.display = 'none';
+            toggleButton.textContent = 'Mostrar LAB';
+        }
+    });
+});
+
+
+
 const filesystem = require('fs');
 
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
