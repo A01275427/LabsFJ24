5f99f7267f01591e6b82d78f58277dde1fa1d9b0
FaKeArtur27 <arsanmx@gmail.com>
2024-03-03 21:30:37 -0600

    algo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log("Hola desde node")
+
 const filesystem = require('fs');
 const http = require('http');
 
@@ -17,11 +19,17 @@ for (let item of arreglo){
 console.log("En que momento se escribe esto")
 */
 
-const server = http.createServer((request, response) => {    
-    if(request.url == "/")
+
+const te_hackie = () => {
+    console.log('jojo te hackié');
+};
+
+
+const server = http.createServer( (request, response) => {    
+    console.log(request.url);
+    if (request.url == "/") {
     response.setHeader('Content-Type', 'text/html');
     response.write(`
-    
     <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -244,13 +252,74 @@ const server = http.createServer((request, response) => {
 </footer>
 </html>
 
-}
 
     `);
     response.end();
-
+    
+    } else if (request.url == "/prueba") {
+    } else {
+      //Código de respuesta para recurso no encontrado
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/html');
+    response.write(`
+    <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorios</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <figure class="figure">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Logo_BMW_Motorrad_2021.svg/2560px-Logo_BMW_Motorrad_2021.svg.png" class="figure-img img-fluid rounded" alt="Logo de BMW Motorrad 2021">
+            </figure>        
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    `);
+    
+    response.end();
+    }
     
 });
+
+    
+
+    
 
 
 server.listen(3000);
