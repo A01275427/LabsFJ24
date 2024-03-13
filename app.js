ef65ca9880ab8c0b354e61f16fa8dce36da4404c
FaKeArtur27 <arsanmx@gmail.com>
2024-03-13 11:55:05 -0600

    mas 12

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')); 
 
