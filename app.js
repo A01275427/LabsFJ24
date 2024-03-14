debb3d356e3c5f682f923b129a5b6770a36106de
FaKeArtur27 <arsanmx@gmail.com>
2024-03-14 04:15:28 -0600

    Lab12

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const motocicletasRoutes = require('./routes/motocicletas.routes');
-const usuariosRoutes = require('./routes/usuarios.routes');
+const usuariosRoutes = require('./routes/usuarios.routes'); 
 
 app.use(motocicletasRoutes);
-app.use(usuariosRoutes);
+app.use(usuariosRoutes); 
 
-app.use((request, response, next) => {
-    response.status(404).render('includes/404', { tituloPagina: 'Página no encontrada' });
+app.use((req, res) => {
+    res.status(404).render('includes/404', { tituloPagina: 'Página no encontrada' });
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
