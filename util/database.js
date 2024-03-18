a5b179d35920f72f1a04e689bdc4ae489dae5841
FaKeArtur27 <arsanmx@gmail.com>
2024-03-18 01:36:15 -0600

    lab17

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'arturosanchezrodiguez_motos',
-    password: 'Dyna=202702'
+    database: 'motos',
+    password: ''
 });
 
 module.exports = pool.promise();
