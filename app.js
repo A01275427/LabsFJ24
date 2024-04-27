690ad46968cb00fcf9998b905c6180799e7a76dc
Arturo <a01275427@tec.mx>
2024-04-27 14:32:05 -0600

    Auth lab18

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const session = require('express-session');
 
 app.use(session({
   secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
-  resave: false, 
-  saveUninitialized: false, 
+  resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+  saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 
 const path = require('path');
@@ -19,17 +19,21 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Agregar protección contra ataques de CSRF
 const csrf = require('csurf');
 const csrfProtection = csrf();
 app.use(csrfProtection); 
 
+//Middleware
 app.use((request, response, next) => {
   console.log('Middleware!');
-  next(); 
+  next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
 const rutasUsuarios = require('./routes/usuarios.routes');
 app.use('/users', rutasUsuarios);
+
+
 
 app.use((request, response, next) => {
   response.status(404);
@@ -38,6 +42,4 @@ app.use((request, response, next) => {
   );
 });
 
-app.listen(3000, () => {
-    console.log('El servidor está corriendo en el puerto 3000');
-  });
+app.listen(3000);
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -4,14 +4,9 @@ const router = express.Router();
 const controllerUsuarios = require('../controllers/usuarios.controller');
 
 router.get('/login', controllerUsuarios.get_login);
-router.post('/login', (req, res, next) => {
-    console.log('Login endpoint hit');
-    next(); 
-  });
+router.post('/login', controllerUsuarios.post_login);
 router.get('/logout', controllerUsuarios.get_logout);
 router.get('/signup', controllerUsuarios.get_signup);
-router.post('/signup', (req, res, next) => {
-    console.log('Signup endpoint hit');
-    next(); 
-  });
+router.post('/signup', controllerUsuarios.post_signup);
+
 module.exports = router;
